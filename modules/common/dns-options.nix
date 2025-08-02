818eeecc8624b8377f0a9bae7245107a339672e5
TheGeneralist <180094941+thegeneralist01@users.noreply.github.com>
2025-08-02 17:04:12 +0200

    dns: remove nextdns
    Tailscale's already using it, and I am using Tailscale 24/7

--- modules/common/dns-options.nix
+++ modules/common/dns-options.nix
@@ -3,8 +3,6 @@
 in {
   options.dnsServers = mkOption {
     default = [
-      "45.90.28.181#365fed.dns.nextdns.io"
-      "45.90.30.181#365fed.dns.nextdns.io"
       "100.100.100.100#shorthair-wall.ts.net"
     ];
   };
