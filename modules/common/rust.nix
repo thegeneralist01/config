26fe71c022816f3c965cc8f518342cdb59f02f67
TheGeneralist <180094941+thegeneralist01@users.noreply.github.com>
2025-08-03 14:58:05 +0200

    packages: add libiconv for mac, fix nvim lsp

--- modules/common/rust.nix
+++ modules/common/rust.nix
@@ -1,16 +1,31 @@
-{ pkgs, ... }: {
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+{
   # TODO: check these out: https://github.com/RGBCube/ncc/blob/86212e148b2642a51814e873a81be73fbc494e86/modules/common/rust.nix#L15-L24
-  environment.systemPackages = with pkgs; [
-    (fenix.complete.withComponents [
-      "cargo"
-      "clippy"
-      "rust-src"
-      "rustc"
-      "rustfmt"
-    ])
-    rust-analyzer-nightly
+  environment = {
+    variables = {
+      LIBRARY_PATH = lib.mkIf (!config.onLinux) <| lib.makeLibraryPath [ pkgs.libiconv ];
+    };
 
-    nixd
-    nixfmt-rfc-style
-  ];
+    systemPackages = with pkgs; [
+      (fenix.complete.withComponents [
+        "cargo"
+        "clippy"
+        "rust-src"
+        "rustc"
+        "rustfmt"
+      ])
+      rust-analyzer-nightly
+
+      # for nil
+      nixd
+      nixfmt-rfc-style
+
+      libiconv
+    ];
+  };
 }
