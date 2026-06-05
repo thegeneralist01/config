9e366fe80b588e39f460bef7df61052b673c9dd3
TheGeneralist <180094941+thegeneralist01@users.noreply.github.com>
2026-06-05 14:23:04 +0200

    Update resolved DNS settings

--- modules/linux/dns.nix
+++ modules/linux/dns.nix
@@ -1,14 +1,13 @@
 { config, lib, ... }: let
-  inherit (lib) mkIf concatStringsSep;
+  inherit (lib) mkIf;
 in {
   services.resolved = mkIf (!config.isServer) {
     enable = true;
 
-    extraConfig = config.dnsServers
-      |> map (server: "DNS=${server}")
-      |> concatStringsSep "\n";
-
-    dnssec     = "true";
-    dnsovertls = "true";
+    settings.Resolve = {
+      DNS = config.dnsServers;
+      DNSSEC = true;
+      DNSOverTLS = true;
+    };
   };
 }
